23263cedf7454ac4ba1de11fbe0010f0f8fd6e27
Ali G Junejo <aligohar_junejo@live.com>
2019-11-17 04:28:22 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -37,25 +37,20 @@ end
 
 join_ingredients(2)
 
-end 
 
 def find_greater_pair(src)
-new_array = []
-  i = 0
-  while i < src.length do
-    new_array.push(src[i].max)
-    i += 1
-  end
-  new_array
+  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+  # Produce a new Array that contains the larger number of each of the pairs
+  # that are in the inner Arrays
+  [ [5,10] , [15,20] , [25,30] , [35,40] ].max
 end
 
 def total_even_pairs(src)
-  total = 0
-  i = 0
-  while i < src.length do
-    if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
-      total += (src[i][0] + src[i][1])
-    end
-    i += 1
-  end
-total
+  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+  # if both numbers in the pair are even, then add both those numbers to the
+  # total
+  #
+  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
+  # the number was even. Review the operator documentation if you've forgotten
+  # this!
+end
