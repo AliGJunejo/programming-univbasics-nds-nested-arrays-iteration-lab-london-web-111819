53a64b79344834fd399400c8bc993cacf0c84f75
Ali G Junejo <aligohar_junejo@live.com>
2019-11-17 04:30:03 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,11 +1,11 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+   new_array = []
+  i = 0
+  while i < src.length do 
+    new_array.push("I love #{src[i][0]} and #{src[i][1]} on my pizza")
+    i += 1
+  end
+  new_array
   
   food_array = [
     ["Food 1","Food 2"],
