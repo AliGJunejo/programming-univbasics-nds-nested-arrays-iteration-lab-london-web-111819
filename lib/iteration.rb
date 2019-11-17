94f449466565cef3cbef747ed3d506cc8e18b008
Ali G Junejo <aligohar_junejo@live.com>
2019-11-17 04:35:54 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -54,5 +54,13 @@ def find_greater_pair(src)
   new_array
 
 def total_even_pairs(src)
-
+total = 0
+  i = 0
+  while i < src.length do
+    if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
+      total += (src[i][0] + src[i][1])
+    end
+    i += 1
+  end
+total
 end
